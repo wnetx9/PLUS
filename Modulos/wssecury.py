b2d63b9d8fa975df1acf22e3b9319c6a7ea52bb0
PhoenixxZ2023 <101994539+PhoenixxZ2023@users.noreply.github.com>
2023-07-12 11:06:48 -0300

    Update wssecury.py

--- Modulos/wssecury.py
+++ Modulos/wssecury.py
@@ -13,7 +13,7 @@ except:
 PASS = ''
 BUFLEN = 8196 * 8
 TIMEOUT = 60
-MSG = 'WebSocket'
+MSG = 'WebSocket Security'
 COR = '<font color="null">'
 FTAG = '</font>'
 MSx = '@scottssh'
@@ -229,7 +229,7 @@ class ConnectionHandler(threading.Thread):
 
 
 def main(host=IP, port=PORT):
-    print "\033[0;34m━"*8,"\033[1;32m PROXY SOCKS","\033[0;34m━"*8,"\n"
+    print "\033[0;34m━"*8,"\033[1;32m WEBSOCKET SECURITY","\033[0;34m━"*8,"\n"
     print "\033[1;33mIP:\033[1;32m " + IP
     print "\033[1;33mPORTA:\033[1;32m " + str(PORT) + "\n"
     print "\033[0;34m━"*10,"\033[1;32m SSHPLUS","\033[0;34m━\033[1;37m"*11,"\n"
