ccb46deeda94619369b4a73ed38e2e1f8b1f4cd2
wnetx9 <141449470+wnetx9@users.noreply.github.com>
2023-08-06 08:48:54 -0300

    Add files via upload

--- Sistema/open.py
+++ Sistema/open.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-# SSHPLUS By @SSH-PLUS
+# SSHPLUS By @WNET
 import socket, threading, thread, select, signal, sys, time
 from os import system
 system("clear")
