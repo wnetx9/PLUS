fb3fa86292900d8c5e28e88c44f4babb8fb1d7f9
wnetx9 <141449470+wnetx9@users.noreply.github.com>
2023-08-19 14:28:33 -0300

    Add files via upload

--- Modulos/proxy.py
+++ Modulos/proxy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-# WebSecurity by @sottssh
+# WebSecurity by @wnet
 import socket, threading, thread, select, signal, sys, time
 from os import system
 system("clear")
@@ -16,7 +16,7 @@ TIMEOUT = 60
 MSG = 'WebSocket'
 COR = '<font color="null">'
 FTAG = '</font>'
-MSx = '@scottssh'
+MSx = '@wnet'
 COx = '<font color="#00FFFF">'
 FTAx = '</font>'
 DEFAULT_HOST = '0.0.0.0:22'
